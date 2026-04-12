b4ba1d7b22bbc06b31da0206f566ee234f98a052
Jeremy Ellis <jeremy@ellisnet.com>
2026-04-12 15:26:51 -0700

    UI looks great.

---
+++ CodeBrixPlatform/JustBetweenUs.Core/Controls/ImagePosition.cs
@@ -0,0 +1,13 @@
+namespace JustBetweenUs.Controls;
+
+/// <summary>
+/// Specifies the position of the image relative to the text
+/// in an <see cref="EmbeddedImageButton"/>.
+/// </summary>
+public enum ImagePosition
+{
+    Left,
+    Top,
+    Right,
+    Bottom
+}
